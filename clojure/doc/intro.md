b00510d5456123ffb07b6aeb1967ff1faea83930
Nikola Plejic <nikola@plejic.com>
2015-02-07 18:52:41 +0100

    Clojure: initial project structure

---
+++ clojure/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-gameoflife
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ clojure/src/clj_gameoflife/core.clj
@@ -0,0 +1,7 @@
+(ns clj-gameoflife.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
