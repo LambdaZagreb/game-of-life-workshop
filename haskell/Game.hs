18edadb79f11e93aadae0b9009522fbf161a76aa
Deni Bertovic <deni@denibertovic.com>
2015-02-22 15:14:28 +0100

    add size helper and fix error

--- haskell/Game.hs
+++ haskell/Game.hs
@@ -2,15 +2,18 @@ module Game where
 
 import           Data.List
 
-
+type GridSize = (Int, Int)
 type Grid = [[Int]]
 type Cell = (Int, Int)
+
+getGridSize :: Grid -> GridSize
+getGridSize g = undefined
 
 createGrid :: Grid -> Grid
 createGrid g = undefined
 
-getCellStatus :: Cell -> Grid
-getCellStatus c = undefined
+getCellStatus :: Cell -> Grid -> Int
+getCellStatus c g = undefined
 
 invertGrid :: Grid -> Grid
 invertGrid g = undefined
