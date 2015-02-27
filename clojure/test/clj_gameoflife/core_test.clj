035c55e3d274d424ab23e61b030b0b6da8b2b688
Nikola Plejic <nikola@plejic.com>
2015-02-27 14:24:43 +0100

    create-empty-world and create-world: tests

--- clojure/test/clj_gameoflife/core_test.clj
+++ clojure/test/clj_gameoflife/core_test.clj
@@ -2,6 +2,28 @@
   (:require [clojure.test :refer :all]
             [clj-gameoflife.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-create-empty-world
+  (testing "Create an empty square world"
+    (let [empty-5-5 (create-empty-world 5 5)]
+      (is (= 5 (count empty-5-5)))
+      (is (= 5 (count (first empty-5-5))))))
+  (testing "Create an empty rectangular world"
+    (let [empty-4-5 (create-empty-world 4 5)]
+      (is (= 5 (count empty-4-5)))
+      (is (= 4 (count (first empty-4-5)))))))
+
+(deftest test-create-world
+  (testing "Create a square world"
+    (let [grid-5-5 (create-world 5 5 #{[2 2] [3 2] [1 0]})]
+      (is (= "X" ((grid-5-5 2) 2)))
+      (is (= "X" ((grid-5-5 3) 2)))
+      (is (= "X" ((grid-5-5 1) 0)))
+      (is (= "." ((grid-5-5 2) 0)))
+      (is (= "." ((grid-5-5 2) 3)))))
+  (testing "Create a rectangular world"
+    (let [grid-5-5 (create-world 5 4 #{[2 2] [3 2] [4 3]})]
+      (is (= "X" ((grid-5-5 2) 2)))
+      (is (= "X" ((grid-5-5 3) 2)))
+      (is (= "X" ((grid-5-5 4) 3)))
+      (is (= "." ((grid-5-5 2) 0)))
+      (is (= "." ((grid-5-5 2) 3))))))
