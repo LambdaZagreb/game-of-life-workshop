71a72bbbcc225d9d026e11a5abc888f2a092b8a6
Deni Bertovic <deni@denibertovic.com>
2015-02-28 15:00:18 +0100

    update some signatures and add Vagrantfile

--- haskell/Game.hs
+++ haskell/Game.hs
@@ -2,18 +2,26 @@ module Game where
 
 import           Data.List
 
+data Status = Dead | Alive deriving (Show, Eq)
+
 type GridSize = (Int, Int)
-type Grid = [[Int]]
+type Grid = [[Status]]
 type Cell = (Int, Int)
 
 getGridSize :: Grid -> GridSize
 getGridSize g = undefined
 
-createGrid :: Grid -> Grid
+int2Status :: Int -> Status
+int2Status e = undefined
+
+createGrid :: [[Int]] -> Grid
 createGrid g = undefined
 
-getCellStatus :: Cell -> Grid -> Int
+getCellStatus :: Cell -> Grid -> Maybe Status
 getCellStatus c g = undefined
+
+flipCellStatus :: Status -> Status
+flipCellStatus s = undefined
 
 invertGrid :: Grid -> Grid
 invertGrid g = undefined
@@ -24,7 +32,7 @@ isValidCoordinate c g = undefined
 getAliveNeighbourCount :: Cell -> Grid -> Int
 getAliveNeighbourCount c g = undefined
 
-getNextCellStatus :: Cell -> Grid -> Int
+getNextCellStatus :: Cell -> Grid -> Status
 getNextCellStatus c g = undefined
 
 getNextGeneration :: Grid -> Grid
